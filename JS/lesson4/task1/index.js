c8cf075f314d17250785645214b6cf235aa4d500
EvilMangos <59839253+EvilMangos@users.noreply.github.com>
2021-03-26 13:01:25 +0200

    JS:lesson4:hw1

---
+++ JS/lesson4/task1/index.js
@@ -0,0 +1,5 @@
+let sum = 0,
+  n = 199;
+for (let i = 0; i <= n; i++) sum += i;
+
+console.log(`Result: ${sum}`);
---
+++ JS/lesson4/task2/index.js
@@ -0,0 +1,7 @@
+let m = 4,
+  n = 10,
+  result = 1;
+
+for (let i = m; i <= n; i++) if (i % 2) result *= i;
+
+console.log(result);
